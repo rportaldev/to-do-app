646378f747f58e1f4b653cf9dfce42c940672a99
Dev Zero <litkaponii@gmail.com>
2026-03-11 18:24:27 -0500

    Agrego UsuarioRepository con metodo findByEmail

--- to-do-app/src/main/java/com/tuapp/to_do_app/repository/UsuarioRepository.java
+++ to-do-app/src/main/java/com/tuapp/to_do_app/repository/UsuarioRepository.java
@@ -1,5 +1,12 @@
 package com.tuapp.to_do_app.repository;
 
-public interface UsuarioRepository {
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tuapp.to_do_app.model.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+
+	public Optional<Usuario> findByEmail(String email);
 }
